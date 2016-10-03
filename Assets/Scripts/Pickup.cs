2e0eada17b1144ef502a053ce086646c6b7f5070
luko van der maas <lukovandermaas@gmail.com>
2016-10-03 18:28:27 +0200

    made a basic pickup system with parenting, will be improving on this.

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -3,11 +3,52 @@ using System.Collections;
 
 public class Pickup : MonoBehaviour {
 
-    FixedJoint joint;
-    
-	void Start () {
-        joint = GetComponent<FixedJoint>();
-	}
-	
+    GameObject item;
+    SteamVR_TrackedController trackedController;
+
+    void Start() {
+        trackedController = GetComponent<SteamVR_TrackedController>();
+        if (trackedController == null) {
+            Debug.LogError("No tracked controller script on " + this.name);
+        }
+
+        trackedController.TriggerClicked += new ClickedEventHandler(OnTrigerClicked);
+        trackedController.TriggerUnclicked += new ClickedEventHandler(OnTrigerReleased);
+    }
+
+    void OnTrigerClicked(object sender, ClickedEventArgs e) {
+        if (item != null) {
+            item.transform.parent = this.transform;
+            Rigidbody item_rb = item.GetComponent<Rigidbody>();
+            if (item_rb != null) {
+                item_rb.isKinematic = true;
+            }
+        }
+    }
+
+    void OnTrigerReleased(object sender, ClickedEventArgs e) {
+        if (item != null) {
+            item.transform.parent = null;
+            Rigidbody item_rb = item.GetComponent<Rigidbody>();
+            if (item_rb != null) {
+                item_rb.isKinematic = false;
+            }
+            item = null;
+        }
+    }
+
+    void OnTriggerEnter(Collider col) {
+        Debug.Log(col.name + "enterd the trigger");
+        if (col.tag == "interactable") {
+            item = col.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider col) {
+        Debug.Log(col.name + "exited the trigger");
+        if (col.tag == "interactable") {
+            //item = null;
+        }
+    }
 	 
 }
