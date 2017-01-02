5041cd87da6c52443ea8aefbf8dce357ad531701
luko van der maas <lukovandermaas@gmail.com>
2017-01-02 16:08:57 +0100

    Got the configurable joint working sort of

--- Assets/Scripts/Lights/Screwin.cs
+++ Assets/Scripts/Lights/Screwin.cs
@@ -3,6 +3,16 @@ using System.Collections;
 
 public class Screwin : MonoBehaviour {
 
+    [SerializeField]
+    Transform bulbOrigin;
+    [SerializeField]
+    float maxRotation;
+
+    GameObject bulb;
+    Rigidbody bulbRb;
+    Quaternion oldRotation;
+    float rotation = 0f;
+
     ConfigurableJoint cj;
 
 	// Use this for initialization
@@ -10,7 +20,39 @@ public class Screwin : MonoBehaviour {
         cj = GetComponent<ConfigurableJoint>();
 	}
 
+    void FixedUpdate() {
+        if (bulb != null) {
+            if (bulb.transform.position == bulbOrigin.position && bulb.transform.rotation == bulbOrigin.rotation) {
+                cj.connectedBody = bulbRb;
+            }
+
+            if (cj.connectedBody != null) {
+                rotation += Quaternion.Angle(bulb.transform.rotation, oldRotation);
+                if (Quaternion.Angle(bulb.transform.rotation, oldRotation) > 0) {
+                    Debug.Log(Quaternion.Angle(bulb.transform.rotation, oldRotation));
+                }
+
+                Vector3 newPos = Vector3.up * (bulb.transform.position.y + rotation / 10);
+                bulbRb.MovePosition(newPos);
+
+                oldRotation = bulb.transform.rotation;
+            }
+        }
+    }
+
     void OnTriggerEnter(Collider col) {
-        cj.connectedBody = col.transform.parent.GetComponent<Rigidbody>();
+        if (bulb == null) {
+            bulb = col.transform.parent.gameObject;
+            bulbRb = bulb.GetComponent<Rigidbody>();
+
+            bulbRb.MovePosition(bulbOrigin.position);
+            bulbRb.MoveRotation(bulbOrigin.rotation);
+
+            bulbRb.isKinematic = true;
+
+            rotation = 0f;
+            oldRotation = bulb.transform.rotation;
+            Debug.Log(bulb.name + "connected to lamp");
+        }
     }
 }
