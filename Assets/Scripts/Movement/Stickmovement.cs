e6728aeab08f597523a11c63c077bfcce4f98710
luko van der maas <lukovandermaas@gmail.com>
2017-01-21 12:53:07 +0100

    added git lfs and background musix and vase and walking stick

--- Assets/Scripts/Movement/Stickmovement.cs
+++ Assets/Scripts/Movement/Stickmovement.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
+[RequireComponent(typeof(VRTK.VRTK_InteractableObject))]
 public class Stickmovement : MonoBehaviour {
 
     bool use = false;
+
     bool colliding = false;
     Transform playArea;
     Vector3 prevPos;
 
-    void Start() {
-        playArea = VRTK.VRTK_DeviceFinder.PlayAreaTransform();
+    VRTK.VRTK_InteractableObject intObj;
+    List<Collider> cols;
 
+    MeshRenderer rnd;
+
+    void Start() {
         prevPos = transform.position;
+
+        rnd = GetComponentInChildren<MeshRenderer>();
+    }
+
+    void OnEnable() {
+        playArea = VRTK.VRTK_DeviceFinder.PlayAreaTransform();
+        intObj = GetComponent<VRTK.VRTK_InteractableObject>();
+        cols = GetComponentsInChildren<Collider>().ToList<Collider>();
+
+        intObj.InteractableObjectUsed += OnUse;
+        intObj.InteractableObjectUnused += OnUnuse;
+        intObj.InteractableObjectGrabbed += OnGrab;
+        intObj.InteractableObjectUngrabbed += OnUngrab;
+    }
+    
+    void OnDisable() {
+        intObj.InteractableObjectUsed -= OnUse;
+        intObj.InteractableObjectUnused -= OnUnuse;
+        intObj.InteractableObjectGrabbed -= OnGrab;
+        intObj.InteractableObjectUngrabbed -= OnUngrab;
     }
     
     void Update() {
@@ -22,24 +48,41 @@ public class Stickmovement : MonoBehaviour {
     void FixedUpdate() {
         if (use && colliding) {
             Vector3 deltaPos = prevPos - transform.position;
+
             playArea.position = new Vector3(playArea.position.x + deltaPos.x, playArea.position.y, playArea.position.z + deltaPos.z);
+
+            Debug.Log(deltaPos);
         }
+        rnd.material.color = intObj.IsUsing() & colliding ? Color.green : Color.white;
+
         prevPos = transform.position;
     }
 
-    public void OnUse() {
+    void OnUse(object e, VRTK.InteractableObjectEventArgs args) {
         use = true;
     }
 
-    public void OnDisuse() {
+    void OnUnuse(object e, VRTK.InteractableObjectEventArgs args) {
         use = false;
     }
 
-    void OnCollisionEnter() {
+    void OnGrab(object e, VRTK.InteractableObjectEventArgs args) {
+        foreach(Collider col in cols) {
+            col.isTrigger = true;
+        }
+    }
+
+    void OnUngrab(object e, VRTK.InteractableObjectEventArgs args) {
+        foreach (Collider col in cols) {
+            col.isTrigger = false;
+        }
+    }
+
+    void OnTriggerEnter() {
         colliding = true;
     }
 
-    void OnCollisionExit() {
+    void OnTriggerExit() {
         colliding = false;
     }
 
